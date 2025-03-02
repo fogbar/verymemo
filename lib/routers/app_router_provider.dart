60e53a26ddf8d20dfb8be681b7c3365578d94aa8
ejayjeon <ejayjeon@gmail.com>
2025-03-02 16:51:07 +0900

    android version 수정

--- lib/routers/app_router_provider.dart
+++ lib/routers/app_router_provider.dart
@@ -48,7 +48,7 @@ class AppRouter {
       // }
       return null;
     },
-    initialLocation: AppRoute.signup,
+    initialLocation: AppRoute.splash,
   );
 
   static Route? _findRouteByPath(String path) {
