3cab1027c9585c816ff3c8573ee57bef67988be2
ejayjeon <ejayjeon@gmail.com>
2025-03-03 16:15:42 +0900

    permission change

--- lib/features/auth/presentation/providers/auth_provider.dart
+++ lib/features/auth/presentation/providers/auth_provider.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:verymemo/common/configs/storage_key.dart';
 // import 'package:supabase_flutter/supabase_flutter.dart';
